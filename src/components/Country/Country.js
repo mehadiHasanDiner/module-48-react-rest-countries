4fe4d3c5df174f68c798405d00c3f1a3afdd12c2
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-08-29 06:06:04 +0600

    (advanced) add unique key for mapping components

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./Country.css";
 
 const Country = (props) => {
-  const { name, population, area, region } = props.country;
+  const { name, population, area, region, flags } = props.country;
   return (
     <div className="country">
-      <h2>Country Name: {name.common}</h2>
+      <h2>Country: {name.common}</h2>
+      <img src={flags.png} alt="" />
       <p>Population: {population}</p>
       <p>
         <small>Area: {area}</small>
